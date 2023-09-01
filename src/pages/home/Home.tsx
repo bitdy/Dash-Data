3303eee2f4fe0fa328f1cea8a96e2c3967aa1df4
Homayoun <homayoun763@gmail.com>
2023-09-02 00:45:36 +0330

    Box for the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,28 @@
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="box box1">
+      </div>
+      <div className="box box2">
+      </div>
+      <div className="box box3">
+      </div>
+      <div className="box box4">
+      </div>
+      <div className="box box5">
+      </div>
+      <div className="box box6">
+      </div>
+      <div className="box box7">
+      </div>
+      <div className="box box8">
+      </div>
+      <div className="box box9">
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
